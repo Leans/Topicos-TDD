3dc73ceb2cb496b5dde456beed94a2fa3d979b6e
Leans <leans177@gmail.com>
2017-11-27 17:11:59 -0300

    refactorizacion, setter intento1

--- src/Frame.java
+++ src/Frame.java
@@ -4,16 +4,19 @@ public class Frame {
 	private int intento1;
 	private int intento2;
 	
-	public Frame(int p_intento1, int p_intento2) {
+	public Frame(int p_intento1, int p_intento2) {		
+		this.setIntento1(p_intento1);		
+		this.intento2 = p_intento2;
+	}
+	
+	private void setIntento1(int p_intento1) {
 		if (p_intento1 > 10) {
 			this.intento1 = 10;
 		} else {
 			this.intento1 = p_intento1;
 		}
-		
-		this.intento2 = p_intento2;
 	}
-	
+
 	public int getIntento1() {
 		return this.intento1;
 	}
