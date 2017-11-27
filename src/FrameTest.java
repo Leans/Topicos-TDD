226935bd53a258a92bc47c5e0f1ae7e495cd3ce8
Leans <leans177@gmail.com>
2017-11-27 19:09:29 -0300

    testNoEsPleno (verde)

--- src/FrameTest.java
+++ src/FrameTest.java
@@ -56,13 +56,19 @@ public class FrameTest {
 	@Test
 	public void testContabilizarPuntaje() {
 		Frame unFrame = new Frame(2, 5);
-		assertEquals(7, unFrame.ContabilizarPuntaje());		
+		assertEquals(7, unFrame.ContabilizarPuntaje());
 	}
 	
 	@Test
 	public void testEsPleno() {
 		Frame unFrame = new Frame(10, 5);
 		assertTrue(unFrame.esPleno());
+	}
+	
+	@Test
+	public void testNoEsPleno() {
+		Frame unFrame = new Frame(3, 5);
+		assertFalse(unFrame.esPleno());
 	}
 
 }
