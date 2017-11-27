ca28d7a9c17ef58cdcc0273ae7014721ea84b771
Leans <leans177@gmail.com>
2017-11-27 16:58:41 -0300

    testFrame (verde)

---
+++ src/Frame.java
@@ -0,0 +1,20 @@
+
+public class Frame {
+	
+	private int intento1;
+	private int intento2;
+	
+	public Frame(int p_intento1, int p_intento2) {
+		this.intento1 = p_intento1;
+		this.intento2 = p_intento2;
+	}
+	
+	public int getIntento1() {
+		return this.intento1;
+	}
+	
+	public int getIntento2() {
+		return this.intento2;
+	}
+
+}
